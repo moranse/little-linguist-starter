5d5d736d6a5587ba8e7537226a0b0b4ac10f7579
moranse <morani1102@gmail.com>
2024-09-09 20:26:09 +0300

    connecting to firestore

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,23 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withComponentInputBinding()), provideAnimations()]
+  providers: [
+    provideRouter(routes, withComponentInputBinding()),
+    provideAnimations(),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'little-linguist-7a6b6',
+        appId: '1:610314459159:web:c21c329ececb84d22f6452',
+        storageBucket: 'little-linguist-7a6b6.appspot.com',
+        apiKey: 'AIzaSyBmTUk6rx0OL9DoBJuLac_aTAFd9Ytefz0',
+        authDomain: 'little-linguist-7a6b6.firebaseapp.com',
+        messagingSenderId: '610314459159',
+      })
+    ),
+    provideFirestore(() => getFirestore()),
+  ],
 };
